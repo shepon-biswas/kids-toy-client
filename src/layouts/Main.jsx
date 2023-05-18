8dafef898f4b05583b1da0590f49e87bddb541ec
Shepon Biswas <sheponbiswas01@gmail.com>
2023-05-18 11:38:12 +0600

    Footer Added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../pages/shared/Header/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/shared/Footer/Footer';
 
 const Main = () => {
     return (
         <>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </>
     );
 };
