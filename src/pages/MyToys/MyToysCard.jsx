6c2dc5b4ecd511ddacfa13d25a1bae48e972853c
Shepon Biswas <sheponbiswas01@gmail.com>
2023-05-20 03:41:34 +0600

    Implemented delete functionality to delete a single toy data with sweet alert

--- src/pages/MyToys/MyToysCard.jsx
+++ src/pages/MyToys/MyToysCard.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const MyToysCard = ({ toy }) => {
   const {
+    _id,
     name,
     photo,
     sellerName,
@@ -13,10 +15,37 @@ const MyToysCard = ({ toy }) => {
     description,
   } = toy;
 
+  //   Handle Delete function
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/toy/${_id}`, {
+            method: "DELETE"
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "The Toy has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+
   return (
     <>
       <div>
-        <div className="grid grid-cols-9 items-center gap-x-3 my-10 rounded-md border border-[#ffcfec]">
+        <div className="grid grid-cols-6 items-center gap-x-3 my-10 rounded-md border border-[#ffcfec]">
           <div className="col-span-2">
             <img
               src={photo}
@@ -24,10 +53,10 @@ const MyToysCard = ({ toy }) => {
               className="rounded-md shadow-2xl p-2"
             />
           </div>
-          <div className="col-span-6 space-y-2 p-2">
+          <div className="col-span-4 space-y-2 p-2">
             <h4 className="text-2xl font-bold">{name}</h4>
             <p className="text-gray-700">{description}</p>
-            <div className="flex justify-between">
+            <div className="grid grid-cols-1">
               <span>
                 <strong>Seller Name: </strong>
                 {sellerName}
@@ -48,12 +77,18 @@ const MyToysCard = ({ toy }) => {
                 <strong>Rating: </strong>
                 {rating}
               </span>
-            </div>
-          </div>
-          <div className="col-span-1">
-            <div className=" flex flex-col space-y-5 px-3">
-              <button className="btn bg-[#ff0099] border-none"><FaEdit className="me-3"></FaEdit> Edit</button>
-              <button className="btn bg-red-600 border-none"> <FaTrash className="me-3"></FaTrash> Delete</button>
+              <div className=" flex items-center justify-between my-3 gap-3">
+                <button className="btn bg-[#ff0099] border-none w-1/3 ">
+                  <FaEdit className="me-3"></FaEdit> Edit
+                </button>
+                <button
+                  onClick={() => handleDelete(_id)}
+                  className="btn bg-red-600 border-none w-1/3 "
+                >
+                  {" "}
+                  <FaTrash className="me-3"></FaTrash> Delete
+                </button>
+              </div>
             </div>
           </div>
         </div>
