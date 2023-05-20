a88ad6dd0e68084930cf4ac68fff7587e853238d
Shepon Biswas <sheponbiswas01@gmail.com>
2023-05-20 10:49:45 +0600

    Created all toys route & show all data on ALL TOYS page

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import React from "react";
+import { FaEye } from "react-icons/fa";
+import { useLoaderData } from "react-router-dom";
 
 const AllToys = () => {
-    return (
-        <div>
-            <h2>this is all toys page</h2>
+  const allToys = useLoaderData();
+  console.log(allToys);
+  return (
+    <>
+      <div className="w-10/12 mx-auto my-10 ">
+        <div className="overflow-x-auto w-full">
+          <table className="table w-full striped">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Serial</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Sub-Category</th>
+                <th>Seller</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Data Row Start */}
+              {allToys?.map((toy, index) => (
+                <tr key={toy._id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="avatar">
+                      <div className="w-12 rounded-full shadow-md">
+                        <img src={toy.photo} alt="toy-image" />
+                      </div>
+                    </div>
+                  </td>
+                  <td>{toy.name}</td>
+                  <td>{toy.subCategory}</td>
+                  <td>{toy.sellerName}</td>
+                  <td>{toy.price}</td>
+                  <td>{toy.quantity}</td>
+                  <td>
+                    <button className="btn bg-[#ff0099] border-none"> <FaEye className="me-3"></FaEye> View Details</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            {/* Table footer */}
+            <tfoot>
+              <tr>
+                <th>Serial</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Sub-Category</th>
+                <th>Seller</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Action</th>
+              </tr>
+            </tfoot>
+          </table>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default AllToys;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -23,7 +23,8 @@ const router = createBrowserRouter([
             },
             {
                 path:'alltoys',
-                element: <AllToys></AllToys>
+                element: <AllToys></AllToys>,
+                loader: () => fetch(`http://localhost:5000/alltoys`)
             },
             {
                 path:'blogs',
